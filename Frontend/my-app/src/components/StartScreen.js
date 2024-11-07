fad5c8aecdbdacc5228ce2e8033cbc55d558b753
lucilledelbos <lucilledelbos@etud.univ-jfc.fr>
2024-11-07 11:25:14 +0100

    travail0611

--- Frontend/my-app/src/components/StartScreen.js
+++ Frontend/my-app/src/components/StartScreen.js
@@ -1,12 +1,20 @@
 import React from 'react';
 import './StartScreen.css';
-//import image from '../images/lotus.png'; 
 
 const StartScreen = () => {
   return (
 
         <div className="container">
-          <button className="center-button">Commencer une partie</button>
+          <button className="image-button">
+            <img src="/images/info.png" alt="gameRules" className="info" />
+          </button>
+          <img src="/images/logoJeu.png" alt="lotus" className="logo" />
+          
+          <div className='startGame'>
+            <img src="/images/lotus.png" alt="lotus" className="image" />
+            <button className="center-button">Commencer une partie</button>
+            <img src="/images/lotus.png" alt="lotus" className="image" />
+          </div>
         </div>
     );
 };
